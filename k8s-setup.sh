04dbdd19467b7693b7b089a7c810675964682073
Xiaolin (Charlene) Zang <czang@cmu.edu>
2019-11-08 22:50:42 -0500

    rewrite script manifest for node-setup.

--- k8s-setup.sh
+++ k8s-setup.sh
@@ -13,14 +13,15 @@ if [ "$#" -ne 3 ]; then
   exit -1
 fi
 
-# figure out the directory of this script so that other scripts'
-# location is known.
-# copied directly from stackoverflow (many thanks)
+# figure out the directory of this script so that sub scripts can be found.
+# copied directly from stackoverflow (many thanks).
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
   SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  # if $SOURCE was a relative symlink,
+  # we need to resolve it relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
