37b27fff688f5c535e9c1fb6a7f3110307f862db
Xiaolin (Charlene) Zang <czang@cmu.edu>
2019-11-09 21:41:31 -0500

    Comments in the local setup file.

--- k8s-setup.sh
+++ k8s-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# invoke nodeSetup.sh to create a k8s cluster.
-# how to use: ./setup.sh sshKey sshUser "node1 node2 ...".  node1 is master
+# invoke k8s-node-setup.sh to create a k8s cluster.
+# ./k8s-setup.sh sshKey sshUser "node1 node2 ...".  node1 is master
 
 if [[ $EUID -ne 0 ]]; then
   sudo $0 $@
@@ -27,33 +27,33 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 sshKey=$1
 sshUser=$2
-
 sshCmd="ssh -o StrictHostKeyChecking=no -i $sshKey -l $sshUser"
 scpCmd="scp -o StrictHostKeyChecking=no -i $sshKey"
-setupScript="/h/czang/k8s/nodeSetup.sh"
 
 read -ra nodes <<< "$3"
 for i in "${!nodes[@]}"; do
   node="${nodes[$i]}"
 
-  # copy the scripts to node
-  $scpCmd $DIR/nodeSetup.sh ${sshUser}@${node}:/tmp
-  $scpCmd $DIR/localSetup.sh ${sshUser}@${node}:/tmp
+  # copy scripts to node
+  $scpCmd $DIR/k8s-node-setup.sh ${sshUser}@${node}:/tmp
+  $scpCmd $DIR/k8s-local-setup.sh ${sshUser}@${node}:/tmp
 
   if [ $i -eq 0 ]; then
-    # master node. nodeSetup.sh will dump the output of kubeadm init
-    # into $nodeJoinFile (see nodeSetup.sh).  The file will be used by
-    # the worker nodes to join the cluster.
+    # master node. The output of kubeadm init will be dumped into
+    # nodeJoinFile (see k8s-node-setup.sh).  The file will be copied
+    # the worker nodes for it to join the cluster.
 
-    # setup master node, get nodeJoinFile location and scp it from master
-    nodeJoinFileColon=$($sshCmd ${node} "/tmp/nodeSetup.sh \"$3\"" | grep "nodeJoinFile:") || exit 1
+    # run node setup script, get nodeJoinFile's location from output
+    nodeJoinFileColon=$($sshCmd ${node} "/tmp/k8s-node-setup.sh \"$3\"" | grep "nodeJoinFile:") || exit 1
     read -r tag nodeJoinFile <<< "$nodeJoinFileColon"
     $scpCmd ${sshUser}@${node}:$nodeJoinFile /tmp || exit 1
   else
+    # worker node. copy nodeJoinFile to node and run node setup script
+
     nodeJoinFileDir=$(dirname $nodeJoinFile)
     $sshCmd ${node} "mkdir -p \"$nodeJoinFileDir\""
     $scpCmd /tmp/nodeJoinFile ${sshUser}@${node}:$nodeJoinFile || exit 1
-    $sshCmd ${node} "/tmp/nodeSetup.sh" || exit 1
+    $sshCmd ${node} "/tmp/k8s-node-setup.sh" || exit 1
   fi
 done
 
